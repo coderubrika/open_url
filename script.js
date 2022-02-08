a7609dec34d33bea0ca9ae52913e76a51be3d217
Anton Samartsev <samarcev.anton1@gmail.com>
2022-02-09 01:50:38 +0300

    initial

---
+++ script.js
@@ -0,0 +1,51 @@
+(()=>{
+
+
+// const vars = {}
+
+
+window.onload = async () => {
+    if (navigator.serviceWorker) {
+        try {
+            await navigator.serviceWorker.register('sw.js')
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+    }
+}
+
+// window.addEventListener('beforeinstallprompt', event => {
+//     event.preventDefault()
+//     vars['beforeinstallprompt'] = event
+// })
+
+const buttonOpen = document.querySelector('button#open')
+// const buttonInstall = document.querySelector('button#install')
+
+
+const textarea = document.querySelector('textarea')
+
+textarea.value = localStorage.getItem('URL_OPENER__content')
+
+textarea.oninput = ({target}) => {
+    localStorage.setItem('URL_OPENER__content', target.value)
+}
+
+buttonOpen.onclick = ()=> {
+    const text = textarea.value
+    const links = text.split('\n')
+
+    links
+        .filter(link => link !== '')
+        .forEach( link => {
+            window.open(link, '_blank')
+        })
+}
+
+buttonInstall.onclick = async ()=> {
+    vars['beforeinstallprompt'].prompt()
+}
+
+})()
